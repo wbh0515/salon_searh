f365bfefaa938f8d6796ddfb3ea7f4b2f64758b4
wbh0515 <13674145866@163.com>
2025-06-30 07:23:32 +0800

    Add files via upload

--- salon_search.py
+++ salon_search.py
@@ -1,33 +1,83 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "30decac3-9af8-43df-90d2-502482c44dd4",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+
+# In[6]:
+
+
+merged_df = pd.read_csv("merged.csv")
+
+
+# In[8]:
+
+
+st.title("サロンサーチ")
+
+price_limit = st.slider("最低カット価格の上限", min_value=2088, max_value=8500, step=200, value=6000)
+score_limit = st.slider("人気スコアの下限", min_value=0.0, max_value=35.0, step=2.0, value=5.0)
+
+
+# In[11]:
+
+
+filtered_df = merged_df[
+    (merged_df['price'] <= price_limit) &
+    (merged_df['pop_score'] >= score_limit)
+]
+
+
+# In[14]:
+
+
+fig = px.scatter(
+    filtered_df,
+    x='pop_score',
+    y='price',
+    hover_data=['name_salon', 'access', 'star', 'review'],
+    title='人気スコアと最低カット価格の散歩図'
+)
+
+st.plotly_chart(fig)
+
+
+# In[17]:
+
+
+selected_salon = st.selectbox('気になるサロンを選んで詳細を確認', filtered_df['name_salon'])
+
+if selected_salon:
+    url = filtered_df[filtered_df['name_salon'] == selected_salon]['link_detail'].values[0]
+    st.markdown(f"[{selected_salon}のページへ移動]({url})", unsafe_allow_html=True)
+
+
+# In[19]:
+
+
+sort_key = st.selectbox(
+    "ランキング基準を選んでください",
+    ("star", "pop_score", "review", "price", "seats")
+)
+ascending = True if sort_key == "price" else False
+
+
+# In[21]:
+
+
+st.subheader(f"{sort_key} によるサロンランキング(上位10件)")
+ranking_df = filtered_df.sort_values(by=sort_key, ascending=ascending).head(10)
+st.dataframe(ranking_df[["name_salon", "price", "pop_score", "star", "review",
+"seats", "access"]])
+
+
+# In[ ]:
+
+
+
+
